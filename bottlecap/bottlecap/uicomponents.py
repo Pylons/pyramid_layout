d6ed1ac56b01469e6301c5234eced390465400df
Hanno Schlichting <hanno@jarn.com>
2011-08-31 15:37:01 +0200

    Add a silly example for a dynamic part of the topbar, to make it more obvious why there's a view

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -3,4 +3,4 @@ from pyramid.view import view_config
 
 @view_config(name='bottlecap.topbar', renderer='templates/topbar.pt')
 def my_view(request):
-    return {}
+    return {'profile_name': 'My profile'}
