ec567f3205baf0172251568ae5899a6956e68799
Hanno Schlichting <hanno@jarn.com>
2011-09-05 14:40:30 +0200

    Crude way to fully enable development mode

--- bottlecap/bottlecap/application.py
+++ bottlecap/bottlecap/application.py
@@ -5,6 +5,16 @@ from pyramid.config import Configurator
 
 def main():
     config = Configurator()
+    # enable development mode
+    config.registry.settings['debug_templates'] = True
+    config.registry.settings['reload_assets'] = True
+    config.registry.settings['reload_resources'] = True
+    config.registry.settings['reload_templates'] = True
+    config.registry.settings['pyramid.debug_templates'] = True
+    config.registry.settings['pyramid.reload_assets'] = True
+    config.registry.settings['pyramid.reload_resources'] = True
+    config.registry.settings['pyramid.reload_templates'] = True
+    # normal setup
     config.scan("bottlecap")
     config.include(inject_static)
     config.add_static_view('bc-static', 'bottlecap:static/',
