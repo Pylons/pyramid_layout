e9ae6216aeef46d9da798e512c9cabfbb5b6cfa3
Denys Mishunov <denys.mishunov@gmail.com>
2011-09-21 13:22:31 +0200

    registered contextTools ui component

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -2,5 +2,6 @@ from pyramid.view import view_config
 
 
 @view_config(name='bottlecap.topbar', renderer='templates/topbar.pt')
+@view_config(name='bottlecap.contextTools', renderer='templates/context_tools.pt')
 def my_view(request):
     return {'profile_name': 'My profile'}
