c7a2f179452c66dcf3bd2884fde65a3cce4c8b0e
Denys Mishunov <denys.mishunov@gmail.com>
2011-09-22 16:20:55 +0200

    Split the top bar's component into 2 separate for the global navigation and the personal tools

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -1,7 +1,8 @@
 from pyramid.view import view_config
 
 
-@view_config(name='bottlecap.topbar', renderer='templates/topbar.pt')
+@view_config(name='bottlecap.globalnav', renderer='templates/globalnav.pt')
+@view_config(name='bottlecap.personaltools', renderer='templates/personaltools.pt')
 @view_config(name='bottlecap.contextTools', renderer='templates/context_tools.pt')
 def my_view(request):
-    return {'profile_name': 'My profile'}
+    return {'profile_name': 'John Doe'}
