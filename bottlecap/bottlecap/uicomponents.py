43f4415042f474a1c59d93d0ca25e5ab4141ac9b
Denys Mishunov <denys.mishunov@gmail.com>
2011-09-26 15:20:04 +0200

    Moved the searchbox to the separate component

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -2,6 +2,7 @@ from pyramid.view import view_config
 
 
 @view_config(name='bottlecap.globalnav', renderer='templates/globalnav.pt')
+@view_config(name='bottlecap.search', renderer='templates/search.pt')
 @view_config(name='bottlecap.personaltools', renderer='templates/personaltools.pt')
 @view_config(name='bottlecap.contextTools', renderer='templates/context_tools.pt')
 def my_view(request):
