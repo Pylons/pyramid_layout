1b0bc52e9de08b05c3cac35019a0cbafab53588a
Denys Mishunov <denys.mishunov@gmail.com>
2011-09-27 15:36:14 +0200

    Renamed context tools to actionsMenu and added new component for, actually, contextTools as defined in the Balsamiq mockups

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -3,7 +3,8 @@ from pyramid.view import view_config
 
 @view_config(name='bottlecap.globalnav', renderer='templates/globalnav.pt')
 @view_config(name='bottlecap.search', renderer='templates/search.pt')
-@view_config(name='bottlecap.personaltools', renderer='templates/personaltools.pt')
 @view_config(name='bottlecap.contextTools', renderer='templates/context_tools.pt')
+@view_config(name='bottlecap.personaltools', renderer='templates/personaltools.pt')
+@view_config(name='bottlecap.actionsMenu', renderer='templates/actions_menu.pt')
 def my_view(request):
     return {'profile_name': 'John Doe'}
