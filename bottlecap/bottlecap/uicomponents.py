c1fad005b1b337bfe0baba79260a1b63d8d674ba
Denys Mishunov <denys.mishunov@gmail.com>
2011-10-05 16:03:51 +0200

    New component for the column one

--- bottlecap/bottlecap/uicomponents.py
+++ bottlecap/bottlecap/uicomponents.py
@@ -6,5 +6,6 @@ from pyramid.view import view_config
 @view_config(name='bottlecap.contextTools', renderer='templates/context_tools.pt')
 @view_config(name='bottlecap.personalTools', renderer='templates/personal_tools.pt')
 @view_config(name='bottlecap.actionsMenu', renderer='templates/actions_menu.pt')
+@view_config(name='bottlecap.columnone', renderer='templates/columnone.pt')
 def my_view(request):
     return {'profile_name': 'John Doe'}
