58878e58b7822d1b56bcaec740be1a8c3e9d7e75
Paul Everitt <paul@agendaless.com>
2011-11-05 14:57:06 -0400

    Fewer knobs, fix test

--- bottlecap/bottlecap/application.py
+++ bottlecap/bottlecap/application.py
@@ -5,14 +5,14 @@ from pyramid.config import Configurator
 def main():
     config = Configurator()
     # enable development mode
-    config.registry.settings['debug_templates'] = True
+    #config.registry.settings['debug_templates'] = True
     config.registry.settings['reload_assets'] = True
     config.registry.settings['reload_resources'] = True
     config.registry.settings['reload_templates'] = True
-    config.registry.settings['pyramid.debug_templates'] = True
-    config.registry.settings['pyramid.reload_assets'] = True
-    config.registry.settings['pyramid.reload_resources'] = True
-    config.registry.settings['pyramid.reload_templates'] = True
+    #config.registry.settings['pyramid.debug_templates'] = True
+    #config.registry.settings['pyramid.reload_assets'] = True
+    #config.registry.settings['pyramid.reload_resources'] = True
+    #config.registry.settings['pyramid.reload_templates'] = True
     # normal setup
     config.scan("bottlecap")
     from bottlecap import layout
--- bottlecap/bottlecap/tests.py
+++ bottlecap/bottlecap/tests.py
@@ -11,4 +11,4 @@ class FunctionalTests(unittest.TestCase):
 
     def test_root(self):
         res = self.testapp.get('/', status=200)
-        self.assertTrue('tracer' in res.body)
+        self.assertTrue('caption' in res.body)
