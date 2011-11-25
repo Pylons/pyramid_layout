f21609e69f038d345546c49f26d281933c31eb85
Denys Mishunov <denys.mishunov@gmail.com>
2011-11-25 14:59:04 +0100

    JS handling for the quickpanels is back

--- bottlecap/bottlecap/static/js/dropdown.js
+++ bottlecap/bottlecap/static/js/dropdown.js
@@ -1,14 +1,10 @@
 /*globals jQuery:false  */
 (function ($) {
 
-    var d = '.quickpanel-toggle';
-    function clearMenus() {
-        $(d).removeClass('open');
-    }
     $(function () {
         $('#navigation-toggle').add('#search-toggle').bind('click touchstart', function (e) {
             var isActive = $(this).hasClass('open');
-            clearMenus();
+            $('.quickpanel-toggle').removeClass('open');
             if (!isActive) {
                 $(this).toggleClass('open');
                 if(e.currentTarget.id === 'search-toggle') {
@@ -19,5 +15,20 @@
             return false;
         });
     });
+    
+    $(function () {
+        $('body').quickpanel('.quickpanel-toggle');
+    });
+
+    $.fn.quickpanel = function (selector) {
+        return this.each(function () {
+            $(this).delegate(selector, 'click', function (e) {
+                var $li = $(this).parents('[data-quickpanel="quickpanel"]');
+                $('[data-quickpanel]').not($li).removeClass('open');
+                $li.toggleClass('open');
+                return false;
+            });
+        });
+    };
     
 }(jQuery));
