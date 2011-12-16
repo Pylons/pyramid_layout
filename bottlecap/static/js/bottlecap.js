7601a9068abc68498bacc05312c2ee7a2718e3b9
Balazs Ree <ree@greenfinity.hu>
2011-12-16 15:28:22 +0100

    Add the animated panel and bind to the Chatter link

--- bottlecap/static/js/bottlecap.js
+++ bottlecap/static/js/bottlecap.js
@@ -67,4 +67,133 @@
         return false;
     });
 
+
+
+    // --
+    // Component for Chatter
+    // --
+
+    $.widget('bc.chatterpanel', {
+
+        options: {
+            container: 'body',
+            insertMethod: 'insertAfter'
+        },
+
+        show: function(callback) {
+            var self = this;
+            if (this.state != this._STATES.HIDDEN) {
+                // Ignore it if we are not showable.
+                if (callback) {
+                    callback();
+                }
+            } else {
+                // Show it.
+                this.state = this._STATES.TO_VISIBLE;
+                this.element.show();
+                this.element.css('height', '100%');
+                var height = this.element.height();
+                this.element.height(0);
+                this.element
+                    .animate({
+                        height: height
+                    }, 350, function() {
+                        self.state = self._STATES.VISIBLE;
+                        if (callback) {
+                            callback();
+                        }
+                    });
+                
+            }
+        },
+
+        hide: function(callback) {
+            var self = this;
+            if (this.state != this._STATES.VISIBLE) {
+                // Ignore it if we are not hidable.
+                if (callback) {
+                    callback();
+                }
+            } else {
+                // Hide it.
+                this.state = this._STATES.TO_HIDDEN;
+                this.element
+                    .animate({
+                        height: 0
+                    }, 150, function() {
+                        self.state = self._STATES.HIDDEN;
+                        self.element.hide();
+                        if (callback) {
+                            callback();
+                        }
+                    });
+            }
+        },
+
+        toggle: function(callback) {
+            if (this.state == this._STATES.VISIBLE) {
+                this.hide(callback);
+            } else if (this.state == this._STATES.HIDDEN) {
+                this.show(callback);
+            } else {
+                // Ignore it if we are transitioning.
+                if (callback) {
+                    callback();
+                }
+            }
+
+        },
+
+
+        // --
+        // private parts
+        // --
+        
+
+        _STATES: {
+            HIDDEN: 0,
+            TO_VISIBLE: 1,
+            VISIBLE: 2,
+            TO_HIDDEN: 3
+        },
+
+        _create: function() {
+            var self = this;
+            // Fill in the content for now. We will want this to
+            // come from a microtemplate.
+            // For now, we just reposition the tag to the
+            // position as specified in the container option.
+            var content = this.options.content;
+            this.element[this.options.insertMethod](this.options.container);
+            // initialize the content to hidden
+            this.state = this._STATES.HIDDEN;
+            this.element.hide();
+        }
+
+        //destroy: function() {
+        //    // In jQuery UI 1.8, you must invoke the destroy method from the base widget
+        //    $.Widget.prototype.destroy.call( this );
+        //    // In jQuery UI 1.9 and above, you would define _destroy instead of destroy and not call the base method
+        //}
+
+
+    });
+
+
+    // --
+    // Wire chatterpanel in header
+    // --
+    
+    $(function() {
+        $('#chatter-panel').chatterpanel({
+            container: '#top-bar'
+        });
+
+        $('#chatter').click(function() {
+            $('#chatter-panel').chatterpanel('toggle');
+            return false;
+        });
+    });
+
+
 } (jQuery));
