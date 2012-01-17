f02a0ad4865ec4ffdaa48cb0023f42edc38021fb
Chris Rossi <chris@archimedeanco.com>
2012-01-17 12:07:05 -0500

    Start tests for panels.

--- bottlecap/layouts/popper/tests.py
+++ bottlecap/layouts/popper/tests.py
@@ -1,9 +1,12 @@
 try: #pragma NO COVERAGE
     # python < 2.7
     import unittest2 as unittest
+    unittest # pyflakes stfu
 except ImportError: #pragma NO COVERAGE
     # python >= 2.7
     import unittest
+
+from pyramid import testing
 
 
 class TestPopperLayout(unittest.TestCase):
@@ -11,3 +14,21 @@ class TestPopperLayout(unittest.TestCase):
     def make_one(self, context, request):
         from bottlecap.layouts.popper.layout import PopperLayout
         return PopperLayout(context, request)
+
+
+class TestPanels(unittest.TestCase):
+
+    def setUp(self):
+        from bottlecap.config import create_layout_manager
+        config = testing.setUp()
+        config.include('bottlecap')
+        context = None
+        self.request = request = testing.DummyRequest()
+        request.context = context
+        request.request = request
+        create_layout_manager(request)
+
+    def test_personal_tools(self):
+        lm = self.request.layout_manager
+        self.assertTrue(lm.render_panel('popper.personal_tools'))
+
