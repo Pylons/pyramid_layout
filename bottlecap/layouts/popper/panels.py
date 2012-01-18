97395db9f73171b4f447eabc647b2e05f851b6f8
Chris Rossi <chris@archimedeanco.com>
2012-01-18 11:13:38 -0500

    Add letter box panel.

--- bottlecap/layouts/popper/panels.py
+++ bottlecap/layouts/popper/panels.py
@@ -1,5 +1,6 @@
 from bottlecap.panel import panel_config
 
+
 @panel_config(name='popper.global_logo',
               renderer='templates/global_logo.pt')
 def global_logo(context, request):
@@ -7,6 +8,7 @@ def global_logo(context, request):
         logo_href=request.layout_manager.layout.site_title_link,
         logo_title=request.layout_manager.layout.site_title,
     )
+
 
 @panel_config(name='popper.global_nav',
         renderer='templates/global_nav.pt')
@@ -19,10 +21,12 @@ def global_nav(context, request):
         dict(title="Item 5", url='#', selected=None)]
     return {'nav_menu': nav_menu}
 
+
 @panel_config(name='popper.personal_tools',
               renderer='templates/personal_tools.pt')
 def personal_tools(context, request):
     return dict(profile_name="John Doe")
+
 
 @panel_config(name='popper.search', renderer='templates/search.pt')
 @panel_config(name='popper.context_tools',
@@ -30,6 +34,7 @@ def personal_tools(context, request):
 @panel_config(name='popper.column_one', renderer='templates/column_one.pt')
 def generic_panel(context, request):
     return {}
+
 
 @panel_config(name='popper.actions_menu', renderer='templates/actions_menu.pt')
 def action_menu(context, request):
@@ -42,3 +47,12 @@ def action_menu(context, request):
         {'title': 'Edit', 'url': '#'},
         {'title': 'Delete', 'url': '#', 'confirm': 'Are you sure?'}
     ]}
+
+
+@panel_config(name='popper.letter_box', renderer='templates/letter_box.pt')
+def letter_box(context, request):
+    letters = [{'name': chr(ch),
+                'href': '#' if ch in (67, 69, 75) else None,
+                'is_current': True if ch == 65 else False}
+               for ch in xrange(ord('A'), ord('Z') + 1)]
+    return {'letters': letters}
