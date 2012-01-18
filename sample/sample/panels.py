7da65935260b4e450150e2c5ef0277171a1ac2b1
Chris Rossi <chris@archimedeanco.com>
2012-01-18 10:00:34 -0500

    Fix global nav.

--- sample/sample/panels.py
+++ sample/sample/panels.py
@@ -5,9 +5,15 @@ from bottlecap.panel import panel_config
                        '.pt')
 def global_nav(context, request):
     nav_menu = [
-        dict(title="Intranet", url='/', selected=None),
-        dict(title="Communities", url='/communities', selected=None),
-        dict(title="People", url='/peopleosf', selected=None),
+        dict(title="Intranet",
+             url=request.application_url,
+             selected=None),
+        dict(title="Communities",
+             url=request.resource_url(context, 'communities'),
+             selected=None),
+        dict(title="People",
+             url=request.resource_url(context, 'peopleosf'),
+             selected=None),
         dict(title="Calendar", url='#', selected='selected'),
         dict(title="Feed", url='#', selected=None)]
     return {'nav_menu': nav_menu}
