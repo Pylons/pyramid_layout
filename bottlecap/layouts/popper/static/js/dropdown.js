1d77bbb36f78126e2dc61fb66ca03178fadc29ae
Denys Mishunov <denys.mishunov@gmail.com>
2012-01-20 19:26:18 +0100

    If there is not enough space for a dropdown to be located to the right from a toggle, put it to the right.

--- bottlecap/layouts/popper/static/js/dropdown.js
+++ bottlecap/layouts/popper/static/js/dropdown.js
@@ -8,8 +8,18 @@
     $.fn.quickpanel = function (selector) {
         return this.each(function () {
             $(this).delegate(selector, 'click', function (e) {
-                var $par = $(this).parent('[data-quickpanel="quickpanel"]');
-                $('[data-quickpanel]').not($par).removeClass('open');
+                var $par = $(this).parent('[data-quickpanel="quickpanel"]'),
+                    dw = $(document).width(),
+                    w = $par.width(),
+                    l = $par.offset().left,
+                    dropw = $par.find('.dropdown').width(),
+                    r = dw - l - w;
+                $('[data-quickpanel]')
+                    .not($par)
+                    .removeClass('open');
+                if (r < dropw) {
+                    $par.addClass('rtl');
+                }
                 $par.toggleClass('open');
                 if(e.currentTarget.id === 'search-toggle') {
                     $par.find('.search-site-box').focus();
