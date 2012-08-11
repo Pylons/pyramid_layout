73f814d838784164cefc7f5835e6ce6b2dc6bcac
Blaise Laflamme <blaise@laflamme.org>
2012-08-10 18:45:39 -0600

    moved navbar to panel

--- demo/demo/panels.py
+++ demo/demo/panels.py
@@ -2,9 +2,14 @@ from pyramid_layout.panel import panel_config
 
 
 @panel_config(
+    name='navbar',
+    renderer='demo:templates/panels/navbar.mako')
+def navbar(context, request):
+    return {}
+
+
+@panel_config(
     name='hero',
     renderer='demo:templates/panels/hero.mako')
 def hero(context, request, title='Hello, world!'):
     return {'title': title}
-
-
