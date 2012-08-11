96a33d0a788bfba1b32cae925bf6906e666e7d0e
Blaise Laflamme <blaise@laflamme.org>
2012-08-10 18:27:30 -0600

    added bootstrap

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='demo:templates/layout.mako')
 def my_view(request):
     return {'project':'demo'}
